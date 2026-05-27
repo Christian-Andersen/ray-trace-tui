d62aca58d8a38bfdb359d78d9a499a2c492fa390
Christian Andersen <cdandersen98@gmail.com>
2026-05-27 19:09:26 +1000

    added a check step so zls can use it

--- build.zig
+++ build.zig
@@ -4,16 +4,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "ray_trace_tui",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
+    const exe = b.addExecutable(.{
+        .name = "ray_trace_tui",
+        .root_module = exe_mod,
+    });
     b.installArtifact(exe);
+
+    const exe_check = b.addExecutable(.{
+        .name = "ray_trace_tui",
+        .root_module = exe_mod,
+    });
+
+    const check = b.step("check", "Check if ray_trace_tui compiles");
+    check.dependOn(&exe_check.step);
 
     const run_step = b.step("run", "Run the app");
     const run_cmd = b.addRunArtifact(exe);
