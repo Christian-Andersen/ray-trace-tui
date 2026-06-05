0c954561871d01d9663b0554abc626185f2fccf6
Christian Andersen <cdandersen98@gmail.com>
2026-06-05 16:20:20 +1000

    refactoring

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,4 @@
+//! Draws a light simulation to the terminal (you can zoom out for higher resolution)!
 const std = @import("std");
 
 const Point = @Vector(2, f32);
@@ -7,13 +8,43 @@ fn getDistance(v: Point, w: Point) f32 {
     return @sqrt(@reduce(.Add, diff * diff));
 }
 
+const State = struct {
+    emitter: Body,
+    receiver: Body,
+    pub fn next(self: *State, speed: f32) f32 {
+        var new_speed = speed;
+        if (self.emitter.p[0] >= 1.0) {
+            new_speed = -@abs(speed);
+        } else if (self.emitter.p[0] <= 0.0) {
+            new_speed = @abs(speed);
+        }
+        self.emitter.p[0] += new_speed;
+        self.receiver.p[0] -= new_speed;
+        return new_speed;
+    }
+};
+
+const Screen = struct {
+    rows: i32,
+    cols: i32,
+    pub fn init(stdout_file: std.Io.File) !Screen {
+        return getTerminalSizePosix(stdout_file);
+    }
+    pub fn get_point(self: *const Screen, x: i32, y: i32) Point {
+        return .{
+            @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(self.cols)),
+            @as(f32, @floatFromInt(y)) / @as(f32, @floatFromInt(2 * self.rows)),
+        };
+    }
+};
+
 const Body = struct {
     p: Point,
     r: f32,
     i: f32,
 };
 
-fn getTerminalSizePosix(out_file: std.Io.File) !struct { rows: i32, cols: i32 } {
+fn getTerminalSizePosix(out_file: std.Io.File) !Screen {
     var winsize: std.posix.winsize = .{
         .row = 0,
         .col = 0,
@@ -53,39 +84,31 @@ fn isInShadow(point: Point, emitter: Body, receiver: Body) bool {
     return dist_sq < (receiver.r * receiver.r);
 }
 
-fn calculateIntensity(point: Point, emitter: Body, receiver: Body) i32 {
-    if (getDistance(point, receiver.p) <= receiver.r) {
+fn calculateIntensity(point: Point, state: State) i32 {
+    if (getDistance(point, state.receiver.p) <= state.receiver.r) {
         return 0;
     }
-    if (getDistance(point, emitter.p) <= emitter.r) {
+    if (getDistance(point, state.emitter.p) <= state.emitter.r) {
         return 255;
     }
-    const shadow_effect: f32 = if (isInShadow(point, emitter, receiver)) 1.1 else 1;
-    return calculateFalloff(shadow_effect * getDistance(point, emitter.p), emitter.i);
+    const shadow_effect: f32 = if (isInShadow(point, state.emitter, state.receiver)) 1.1 else 1;
+    return calculateFalloff(shadow_effect * getDistance(point, state.emitter.p), state.emitter.i);
 }
 
-fn drawFrame(stdout_file: std.Io.File, stdout: *std.Io.Writer, emitter: Body, receiver: Body) !void {
-    const size_result = getTerminalSizePosix(stdout_file);
-    const rows = if (size_result) |s| s.rows else |_| 32;
-    const cols = if (size_result) |s| s.cols else |_| 64;
+/// Gets the dimension of the terminal, and uses the `state` to calculate the intensity, drawing the character to Io.
+fn drawFrame(stdout: *std.Io.Writer, screen: Screen, state: State) !void {
     try stdout.print("\x1b[H", .{});
     var y: i32 = 0;
-    while (y < rows) : (y += 1) {
+    while (y < screen.rows) : (y += 1) {
         if (y != 0) {
             try stdout.print("\n", .{});
         }
         var x: i32 = 0;
-        while (x < cols) : (x += 1) {
-            const top_point = .{
-                @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(cols)),
-                @as(f32, @floatFromInt(2 * y)) / @as(f32, @floatFromInt(2 * rows)),
-            };
-            const top_intensity = calculateIntensity(top_point, emitter, receiver);
-            const bottom_point = .{
-                @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(cols)),
-                @as(f32, @floatFromInt((2 * y) + 1)) / @as(f32, @floatFromInt(2 * rows)),
-            };
-            const bottom_intensity = calculateIntensity(bottom_point, emitter, receiver);
+        while (x < screen.cols) : (x += 1) {
+            const top_point = screen.get_point(x, 2 * y);
+            const bottom_point = screen.get_point(x, (2 * y) + 1);
+            const top_intensity = calculateIntensity(top_point, state);
+            const bottom_intensity = calculateIntensity(bottom_point, state);
             try stdout.print("\x1b[38;2;{d};{d};{d}m\x1b[48;2;{d};{d};{d}m▀\x1b[0m", .{
                 top_intensity,
                 top_intensity,
@@ -98,7 +121,9 @@ fn drawFrame(stdout_file: std.Io.File, stdout: *std.Io.Writer, emitter: Body, re
     }
     try stdout.flush();
 }
+
 pub fn main(init: std.process.Init) !void {
+    // variable setup
     const io = init.io;
     var stdout_buf: [4096]u8 = undefined;
     var stdout_file = std.Io.File.stdout();
@@ -106,25 +131,20 @@ pub fn main(init: std.process.Init) !void {
     const stdout = &stdout_writer.interface;
     try stdout.print("\x1b[?1049h\x1b[?25l\x1b[?7l", .{});
     try stdout.flush();
+    // state setup
     const target_frame_time_ns = (1000 * std.time.ns_per_ms) / 60;
-    var emitter = Body{ .p = .{ 0, 0.5 }, .r = 0.01, .i = 0.5 };
-    var receiver = Body{ .p = .{ 1, 0.3 }, .r = 0.01, .i = 0 };
+    var state = State{ .emitter = Body{ .p = .{ 0, 0.5 }, .r = 0.01, .i = 0.5 }, .receiver = Body{ .p = .{ 1, 0.3 }, .r = 0.01, .i = 0 } };
     var speed: f32 = 0.01;
+    // play loop
     while (true) {
-        if (emitter.p[0] >= 1.0) {
-            speed = -@abs(speed);
-        } else if (emitter.p[0] <= 0.0) {
-            speed = @abs(speed);
-        }
-        emitter.p[0] += speed;
-        receiver.p[0] -= speed;
         const start_time = std.Io.Timestamp.now(io, .awake);
-        try drawFrame(stdout_file, stdout, emitter, receiver);
+        speed = state.next(speed);
+        const screen = try Screen.init(stdout_file);
+        try drawFrame(stdout, screen, state);
+        // wait to cap frame rate
         const elapsed_ns = start_time.untilNow(io, .awake).toNanoseconds();
         if (elapsed_ns < target_frame_time_ns) {
-            const sleep_time_ns = target_frame_time_ns - elapsed_ns;
-            const sleep_duration = std.Io.Duration.fromNanoseconds(sleep_time_ns);
-            try io.sleep(sleep_duration, .awake);
+            try io.sleep(std.Io.Duration.fromNanoseconds(target_frame_time_ns - elapsed_ns), .awake);
         }
     }
 }
